c4e2b15ea3a23c7c98a9b46fcb2d07de9cb8cca8
SAEBOMPAK <saebompak@gmail.com>
2022-01-13 17:05:04 -0800

    Updates

---
+++ precache-manifest.b3d84d607c5dc6550bb0c66a4ec3517a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37ce3052d39ff1ac946a0bb845cc7c4",
+    "url": "/smart_movie_finder/index.html"
+  },
+  {
+    "revision": "0eb6501b7a027057c1b1",
+    "url": "/smart_movie_finder/static/css/main.2bebbff4.chunk.css"
+  },
+  {
+    "revision": "dbed217a0a50e772ea4c",
+    "url": "/smart_movie_finder/static/js/2.ec28df17.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/smart_movie_finder/static/js/2.ec28df17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb6501b7a027057c1b1",
+    "url": "/smart_movie_finder/static/js/main.4aef04d5.chunk.js"
+  },
+  {
+    "revision": "eff103cee50fbc394c3d",
+    "url": "/smart_movie_finder/static/js/runtime-main.d8938f5a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smart_movie_finder/precache-manifest.6ae03e22281e8e43c1d2352e8643aac9.js"
+  "/smart_movie_finder/precache-manifest.b3d84d607c5dc6550bb0c66a4ec3517a.js"
 );
 
 self.addEventListener('message', (event) => {
